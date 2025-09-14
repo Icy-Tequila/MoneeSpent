334cb569cdb5a78a7b64ec0fb713e61067706b26
Icy-Tequila <janeilletrixiedeleon@gmail.com>
2025-09-14 17:54:41 +0800

    feat(page): add bottom navigation tabs for dashboard, expenses, and history

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import { Button } from "@/components/ui/button"
+import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="w-full h-[100dvh]">
+      <Tabs defaultValue="dashboard" className="w-full flex flex-col">
+        <div className="w-full m-3">
+          <TabsContent value="dashboard">Dasboard</TabsContent>
+          <TabsContent value="expenses">Expenses</TabsContent>
+          <TabsContent value="history">History</TabsContent>
+        </div>
+        <div className="flex justify-center">
+          <TabsList className="w-[95%] h-13 gap-2 p-2 fixed bottom-2">
+            <TabsTrigger value="dashboard" className="cursor-pointer">
+              Dashboard
+            </TabsTrigger>
+            <TabsTrigger value="expenses" className="cursor-pointer">
+              Expenses
+            </TabsTrigger>
+            <TabsTrigger value="history" className="cursor-pointer">
+              History
+            </TabsTrigger>
+          </TabsList>
+        </div>
+      </Tabs>
     </div>
-  )
+  );
 }
