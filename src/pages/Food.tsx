39ceffc71be19d577e7f32a3d4df82a51cb31456
Icy-Tequila <janeilletrixiedeleon@gmail.com>
2025-09-14 18:22:21 +0800

    feat(ui): installed popover and calendar component of shadcn for date picker component

--- src/pages/Food.tsx
+++ src/pages/Food.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Food() {
   return (
-    <div>
+    <div className="border border-gray-200 rounded-lg shadow-xs p-3">
       Food Expenses
     </div>
-  )
+  );
 }
