fa02790361b8238356a7664e3ab27a8297434b35
Icy-Tequila <janeilletrixiedeleon@gmail.com>
2025-09-14 18:34:59 +0800

    feat(food): implement date picker component

--- src/pages/Food.tsx
+++ src/pages/Food.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { DatePicker } from "../components/ui/date-picker";
 
 export default function Food() {
   return (
     <div className="border border-gray-200 rounded-lg shadow-xs p-3">
       Food Expenses
+      <DatePicker />
     </div>
   );
 }
