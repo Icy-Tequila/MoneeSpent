5d2d6d6c48285733522f9a387b4c2b5dca20429e
Icy-Tequila <janeilletrixiedeleon@gmail.com>
2025-09-14 19:14:11 +0800

    feat(food): add peso placeholder for amount and "+" symbol in add button

--- src/pages/Food.tsx
+++ src/pages/Food.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { DatePicker } from "../components/ui/date-picker";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
 
 export default function Food() {
   return (
@@ -12,12 +13,18 @@ export default function Food() {
       </div>
       <div className="flex flex-col gap-1">
         <label htmlFor="amount">Amount</label>
-        <Input className="focus:ring-2! text-sm" />
+        <div className="relative">
+          <span className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400">
+            ₱
+          </span>
+          <Input placeholder="0.00" className="pl-7 placeholder-gray-400!" />
+        </div>
+
         <label htmlFor="amount">Note</label>
         <Input className="focus:ring-2! text-sm" />
       </div>
       <div className="flex justify-end mt-3">
-        <Button className="font-normal">Add Expense</Button>
+        <Button className="font-normal cursor-pointer">+ Add Expense</Button>
       </div>
     </div>
   );
