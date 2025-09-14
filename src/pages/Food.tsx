e0cd09e931f110967d7aa105424054bbcb3bee7a
Icy-Tequila <janeilletrixiedeleon@gmail.com>
2025-09-14 20:59:04 +0800

    feat(food): implement dropdown menu for editing and deleting each expense

--- src/pages/Food.tsx
+++ src/pages/Food.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import { DatePicker } from "../components/ui/date-picker";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { EllipsisVertical } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Food() {
   return (
@@ -30,10 +39,56 @@ export default function Food() {
       <div className="border border-gray-200 rounded-lg shadow-xs p-3 mt-3">
         <h2>Food expenses on Sept. 14, 2025</h2>
         <div className="w-full h-[0.5px] bg-gray-200 my-3"></div>
-        <ul className="list-disc ml-5 my-3">
+        <ul className="list-disc ml-5 my-3 flex flex-col gap-2">
+          <div className="flex justify-between">
+            <li>₱60 - Breakfast Bread</li>
+            <DropdownMenu>
+              <DropdownMenuTrigger className="cursor-pointer">
+                <EllipsisVertical />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="mr-10">
+                <DropdownMenuItem className="cursor-pointer">
+                  Edit
+                </DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer text-red-700 hover:text-red-700!">
+                  Delete
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+
+          <div className="flex justify-between">
             <li>₱120 - Lunch Jollibee</li>
-            <li>₱80  - Breakfast</li>
-            <li>₱60  - Snack</li>
+            <DropdownMenu>
+              <DropdownMenuTrigger className="cursor-pointer">
+                <EllipsisVertical />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="mr-10">
+                <DropdownMenuItem className="cursor-pointer">
+                  Edit
+                </DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer text-red-700 hover:text-red-700!">
+                  Delete
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+          <div className="flex justify-between">
+            <li>₱80 - Bingsu</li>
+            <DropdownMenu>
+              <DropdownMenuTrigger className="cursor-pointer">
+                <EllipsisVertical />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="mr-10">
+                <DropdownMenuItem className="cursor-pointer">
+                  Edit
+                </DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer text-red-700 hover:text-red-700!">
+                  Delete
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         </ul>
         <div className="w-full h-[0.5px] bg-gray-200 my-3"></div>
         <h3 className="font-bold flex justify-end">Total: ₱260</h3>
